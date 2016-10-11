d57f8df0ed1ccbb263656c7ec2d41005c7c39e07
nora-yauricaza <nora-yauricaza>
2016-10-10 21:07:33 -0500

    modificando js

--- js/main.js
+++ js/main.js
@@ -3,32 +3,51 @@ window.addEventListener("load",function(){
 
 	var chat=document.getElementById("chat");
 	var mensajes =document.getElementById("mensajes");
-	var conversacion=document.getElementById("conversacion");
-	var pTexto=document.createElement("p");
-	var hora=document.createElement("div");
+	
 
-	pTexto.setAttribute("div", "pTexto");
-	hora.setAttribute("div","hora")
-	hora.classList.add("time");
-	conversacion.appendChild(pTexto);
-	conversacion.appendChild(hora);
-	chat.appendChild(conversacion);
-
-	mensajes.addEventListener("click", function(e){
+	mensajes.addEventListener("keyup", function(e){
 	e.preventDefault();
-		var valor=parseFloat(pTexto.value);
-		indice.push(valor);
-		valor.value="";
-		imprimir();
+		var teclaEnter = e.keyCode;
+			if (teclaEnter==13) {
+				publicacion();
+			}
 	});
 
-	function imprimir(){
-		var conversacion=document.getElementById("conversacion");
-		var pTexto=document.getElementById("pTexto");
-		var hora=document.getElementById("hora");
+	function publicacion(){
+		var contenedor=document.createElement("contenedor");
+		var divPubVerde=document.createElement("div");
+		var pTexto=document.createElement("p");
+		var divHora=document.createElement("div");
+		var horaPub = fechaPubicacion();
 
-		mensajes.innerHTML=indice;
+		pTexto.textContent=mensajes.value;
+		divHora.textContent=horaPub;
+		
+		contenedor.classList.add("w-message","w-message-out");
+		divPubVerde.classList.add("w-message-text");
+		divHora.classList.add("time");
+
+		chat.appendChild(contenedor);
+		contenedor.appendChild(divPubVerde);
+		divPubVerde.appendChild(pTexto);
+		divPubVerde.appendChild(divHora);	
 	}
+
+		function fechaPubicacion(){
+			var fecha= new Date();
+			var hora= fecha.getHours();
+			var minuto=fecha.getMinutes();
+			var segundo=fecha.getSeconds();
+			var horaPub= hora + ":" + minuto+ ":" + segundo;
+			return horaPub;
+		}
+
+	// function cambioImg(){
+	// 	imagen.src=this.children[]
+
+	// 	mensajes.innerHTML=indice;
+	// }
+
 
 });
 
